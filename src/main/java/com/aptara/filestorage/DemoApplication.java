a76d9582411c2bfeb9cd41d77ce6eeea0c7f19b8
shrikantarchit <109203106+shrikantarchit@users.noreply.github.com>
2023-05-08 01:27:49 +0530

    Update DemoApplication.java

--- src/main/java/com/aptara/filestorage/DemoApplication.java
+++ src/main/java/com/aptara/filestorage/DemoApplication.java
@@ -14,6 +14,7 @@ public class DemoApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("dev change");
 	}
 
 	 @Override
